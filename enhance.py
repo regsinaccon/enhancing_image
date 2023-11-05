34169dcba3e81783bbd4a727c88858ad288d7a9f
regsinaccon <116173314+regsinaccon@users.noreply.github.com>
2023-11-05 21:18:59 +0800

    Add files via upload
    an preparation for next stage only contains extracting image but have not yet successfully turn it into a grayscale image file

---
+++ enhance.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy 
+import time
+
+#sigma is approximately 63.5039
+#-2   -1   0     1      2  
+#0.5  64   127.5  191.5  254.5
+def float_to_interger(x:float):
+    if(x - int(x) >= 0.5):
+        return int(x+1)
+    else:
+        return int(x)
+
+
+def rgb_to_gray(red:int,green:int,blue:int):
+    sum=red*0.3+green*0.587+blue*0.114
+    return float_to_interger(sum)
+
+
+
+numbers_of_pixels=0
+pixels_sum_value=0    
+start=time.time()
+gray_list=numpy.empty((0,),dtype=numpy.int8)
+with Image.open('image2.png') as img:
+    img_array = numpy.array(img)
+
+for line in img_array:
+    for pix in line:
+        pixel_value=rgb_to_gray(pix[0],pix[1],pix[2])
+        gray_list=numpy.append(gray_list,pixel_value)
+        numbers_of_pixels += 1
+        pixels_sum_value +=pixel_value
+prime_avg=float_to_interger(pixels_sum_value/numbers_of_pixels)
+# print(prime_avg)
+# end=time.time()
+# print(-(start-end))
+# saved_img=Image.fromarray(gray_list)
+# saved_img.save('new_image2.png')
