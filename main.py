4b598170b13819a05f200b5eb72ef9c2cae11a37
regsinaccon <116173314+regsinaccon@users.noreply.github.com>
2024-02-09 13:51:30 +0800

    Update main.py
    add edge finding funtion

--- main.py
+++ main.py
@@ -39,12 +39,14 @@ def rating_values(len_of_arr,value_array,PRarray,balance):
     PRarray[0]=PRarray[0]/len_of_arr
     for i in range (1,256):
         PRarray[i] =PRarray[i]/len_of_arr +PRarray[i-1]
-        if PRarray[i] - PRarray[i-1]>=0.1:
-            PRarray -= balance
+        if PRarray[i] - PRarray[i-1]>=balance:
+            PRarray[i] = balance + PRarray[i-1]
     return PRarray
 
 def set_figure(array,save_to,title_name,Bins=256,color='gray'):
     plt.title(title_name)
+    plt.xlabel('Gray scale value')
+    plt.ylabel('number of pixels')
     plt.hist(array.ravel(),bins=Bins,color=color,range=(0,255))
     plt.savefig(save_to)
 
@@ -64,28 +66,45 @@ def median_filter(input_array):
             output_array[i][j] = numpy.sort(neighbors)[4]
 # sorted function invalid in jit
     return output_array
+ 
+
 
 @numba.jit(nopython=True)
-def sharp_image(input_array):
+def sharp_image(input_array,Excute_type):
     m, n = input_array.shape
     output_array = numpy.zeros((m,n),dtype=numpy.int32)
-    for i in range(1,m-1):
-        for j in range(1,n-1):
-            sumval = 0
-            for k in range(max(1, i-1), min(i+2, m-1)):
-                for l in range(max(1, j-1), min(j+2, n-1)):
-                    sumval -= input_array[k][l] 
-            output_array[i][j] = sumval+(input_array[i][j]*6)
+    if Excute_type == 0:
+        for row in range(1,m-1):
+            for column in range(1,n-1):
+                    output_array[row][column] += input_array[row][column-1] -input_array[row][column+1]
+    elif Excute_type == 1:
+        for row in range(1,m-1):
+            for column in range(1,n-1):
+                    output_array[row][column] += input_array[row-1][column] -input_array[row][column+1]
 #mask size:1
     return output_array
+# @numba.jit(nopython=True)
+def Combine(array):
 
+    Excute_type = 0
+    first_array = sharp_image(array,Excute_type=Excute_type)
+
+    Excute_type+=1
+    second_array = sharp_image(array,Excute_type=Excute_type)
+    rows,columns = array.shape
+    output_array = numpy.zeros((rows,columns),dtype=numpy.int32)
+    for row in range(rows):
+        for column in range(columns):
+            output_array[row][column] = (first_array[row][column]+second_array[row][column])/2
+
+    return output_array
 if __name__=="__main__":
-    open_image = input("select the input image file path:")
+    open_image = input("Select the input image file path:")
     while os.path.exists(open_image)!=True:
-        open_image=input("file path error ,please try again:")
-    save_to = input("select the file path that the image be saved:")
+        open_image=input("File path error ,please try again:")
+    save_to = input("Select the file path that the image be saved:")
     
-    to_sharp = input("want to make bigger contrast(yes/no):")
+    to_sharp = input("Do you want to show edges(yes/no):")
         
     start = time.time()
     image=Image.open(open_image) 
@@ -101,18 +120,18 @@ if __name__=="__main__":
     gray_values = extract_to_gray(origin_values,gray_values,PRarray,height,width)
     gray_image = Image.fromarray(gray_values.astype('uint8'))
     gray_image.save(save_to)
-    set_figure(gray_values,'figure2','origin image',color='red')
+    set_figure(gray_values,'figure2','Histogram of the image(unprocessed)',color='red')
         
-    PRarray = rating_values(len_of_arr,gray_values,PRarray,balance=0.03)
+    PRarray = rating_values(len_of_arr,gray_values,PRarray,balance=0.015)
     gray_values = process_image(gray_values,PRarray,height,width)
 
-    # gray_values = median_filter(gray_values)  
+    # gray_values = median_filter(gray_values)
 
     if to_sharp=='yes':
-        gray_values = sharp_image(gray_values)
+        gray_values = Combine(gray_values)
 
 
     gray_image = Image.fromarray(gray_values.astype('uint8'))
     gray_image.save(save_to)
-    set_figure(gray_values,'figure1','after processed',color='green')
+    set_figure(gray_values,'figure1','Histogram of the image (unprocessed and after processed)',color='green')
     print(f"process done after {time.time()-start} second")    
